adc3cbb46efb1303d919696ecf62ead99af0a569
Alexandr Kononuk <49821742+mccuyper@users.noreply.github.com>
2021-08-15 15:32:26 +0300

    Update views.py
    search func

--- app/views.py
+++ app/views.py
@@ -3,4 +3,9 @@ from app.models import FetchIt
 
 def showdata(request):
     results = FetchIt.objects.all()
+    item_name = request.GET.get('item_name')
+
+    if item_name != '' and item_name is not None:
+        results = results.filter(d_name__icontains=item_name, direction__icontains='out') # shows up all outgoing direction
+   
     return render(request, 'index.html',{"data":results})
